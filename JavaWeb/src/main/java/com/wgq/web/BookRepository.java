678680290b4c343cb01480a2bb181b28ed44ba24
wuguoquan <zjuwuguoquan@gmail.com>
2020-02-09 00:10:35 +0800

    【提交信息】新增JavaWeb，处理数据库以及邮件业务 【提交人员】wuguoqua

---
+++ JavaWeb/src/main/java/com/wgq/web/BookRepository.java
@@ -0,0 +1,12 @@
+package com.wgq.web;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource(path="books")
+public interface BookRepository extends JpaRepository<Book, Integer> {
+
+}
---
+++ JavaWeb/src/test/java/com/wgq/web/test/SendmailApplicationTests.java
@@ -0,0 +1,66 @@
+package com.wgq.web.test;
+
+import com.wgq.web.MailService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.io.File;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SendmailApplicationTests {
+
+    @Autowired
+    MailService mailService;
+
+    @Test
+    public void sendMailWithImg() {
+        mailService.sendMailWithImg("13433956705@163.com",
+                "13433956705@163.com",
+                "测试邮件主题(图片)",
+                "<div>hello,这是一封带图片资源的邮件：" +
+                        "这是图片1：<div><img src='cid:p01'/></div>" +
+                        "这是图片2：<div><img src='cid:p02'/></div>" +
+                        "</div>",
+                new String[]{"C:\\Users\\wuguoquan\\Desktop\\1.png",
+                             "C:\\Users\\wuguoquan\\Desktop\\2.png"},
+                new String[]{"p01", "p02"});
+    }
+
+    @Test
+    public void sendAttachFileMail() {
+        mailService.sendAttachFileMail("13433956705@163.com",
+                "13433956705@163.com",
+                "测试邮件主题",
+                "测试邮件内容",
+                new File("D:\\GitProject\\123.txt"));
+    }
+
+    @Test
+    public void sendSimpleMail() {
+        mailService.sendSimpleMail("13433956705@163.com",
+                "13433956705@163.com",
+                "13433956705@163.com",
+                "测试邮件主题",
+                "测试邮件内容");
+    }
+
+    @Autowired
+    TemplateEngine templateEngine;
+    @Test
+    public void sendHtmlMailThymeleaf() {
+        Context ctx = new Context();
+        ctx.setVariable("username", "sang");
+        ctx.setVariable("gender", "男");
+        String mail = templateEngine.process("mailtemplate.html", ctx);
+        mailService.sendHtmlMail("13433956705@163.com",
+                "13433956705@163.com",
+                "测试邮件主题",
+                mail);
+    }
+}
